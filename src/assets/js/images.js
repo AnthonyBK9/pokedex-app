3847568b067db61a58b20f600423a77af3df6e40
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2022-06-25 14:13:05 -0500

    creacion de los stilos en la página principaÃl

---
+++ src/assets/js/images.js
@@ -0,0 +1,18 @@
+import men from '../img/men.png'
+import woman from '../img/woman.png'
+import pokemon from '../img/pokemon.png'
+
+export default [
+    {
+        name: 'Pokemon',
+        img: pokemon
+    },
+    {
+        name: 'Men',
+        img: men,
+    },
+    {
+        name: 'Woman',
+        img: woman
+    }
+]
--- src/components/home/HomeScreen.jsx
+++ src/components/home/HomeScreen.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import InputHome from './InputHome'
+import images from '../../assets/js/images'
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>Podedex</h1>
-      <h2>¡Hola Entrenador!</h2>
-      <InputHome />
+    <div className="pokedex">
+      <div className="pokecoach-men">
+        <img src={images[1].img} alt={images[1].name} />
+      </div>
+      <div className="pokedex-intro">
+        <h1 className="pokedex-title">Pokedex</h1>
+        <h4 className="pokedex-content">¡Hola Entrenador!</h4>
+        <InputHome />
+      </div>
+      <div className="pokecoach-woman">
+        <img src={images[2].img} alt={images[2].name} />
+      </div>
     </div>
   )
 }
