c18f3fdfdae50b508ad5f4fe9c331726d2396f0b
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2022-06-27 17:35:09 -0500

    Agregando colores dependiendo el tipo de pokemon v1

--- src/components/pokedex/PokeCard.jsx
+++ src/components/pokedex/PokeCard.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import bgColor from '../../helpers/bgColor'
 // import PokeStats from './PokeStats'
 
 const PokeCard = ({url}) => {
-
+  
   const [pokemon, setPokemon] = useState()
 
   useEffect(() => {
@@ -16,15 +17,27 @@ const PokeCard = ({url}) => {
   const navigate = useNavigate()
   const clickCard = () => navigate(`/pokemon/${pokemon.id}`)
   // console.log(pokemon)
-  
+  bgColor.forEach(bg => {
+    if (pokemon?.types[0].type.name === bg.bgColor) {
+        return `card-pokemon bg-${bg.bgColor}`;
+    } else {  
+      return 'card-pokemon'
+    }
+  });
   return (
-    <article onClick={clickCard} className='card-pokemon'>
+    <article onClick={clickCard} className={(pokemon?.types[0].type.name === bgColor[11].bgColor )? `card-pokemon bg-${bgColor[11].bgColor}`: 'card-pokemon'}>
       <div className="card-poke-header">
-        <img src={pokemon?.sprites.other['official-artwork'].front_default} alt={pokemon?.name} />
-        <h3>{pokemon?.name}</h3>
-        <h4>Type</h4>
-        <p>{pokemon?.types[0].type.name}    {pokemon?.types[1]?.type?.name}</p>
-        {/* <p>{pokemon?.types[1].type.name}</p> */}
+        <div className="card-img">
+          <img src={pokemon?.sprites.other['official-artwork'].front_default} alt={pokemon?.name} />
+        </div>
+        <div className="card-content">
+          <h3>{pokemon?.name}</h3>
+          <h4>Type</h4>
+          <div className="card-type">
+            <p className="type-a">{pokemon?.types[0].type.name} </p>
+            <p className="type-b">{pokemon?.types[1]?.type?.name}</p>
+          </div>
+        </div>
       </div>
         <div className="stats">
           <div className="stat">
