e6aae65f84b1b6570d256400b299a43d3a425f81
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2022-10-30 17:10:16 -0600

    Arreglando el responsive en home.jsx

--- src/components/home/HomeScreen.jsx
+++ src/components/home/HomeScreen.jsx
@@ -5,17 +5,19 @@ import images from '../../assets/js/images'
 const HomeScreen = ({setLogged}) => {
   return (
     <div className="pokedex">
-      <div className="pokecoach-men">
-        <img src={images[1].img} alt={images[1].name} />
-      </div>
-      <div className="pokedex-intro">
-        <img src={images[3].img} alt={images[3].name} />
-        <h4 className="poke-content">Hello Trainer!</h4>
-        <InputHome setLogged={setLogged}/>
-      </div>
-      <div className="pokecoach-woman">
-        <img src={images[2].img} alt={images[2].name} />
-      </div>
+      <dic className="pokedex-content">
+        <div className="pokecoach-men">
+          <img src={images[1].img} alt={images[1].name} />
+        </div>
+        <div className="pokedex-intro">
+          <img src={images[3].img} alt={images[3].name} />
+          <h4 className="poke-content">Hello Trainer!</h4>
+          <InputHome setLogged={setLogged}/>
+        </div>
+        <div className="pokecoach-woman">
+          <img src={images[2].img} alt={images[2].name} />
+        </div>
+      </dic>
     </div>
   )
 }
